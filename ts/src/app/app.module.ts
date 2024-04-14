e3bda6703d5615d28ceeaceb364dd5025ccdca4c
annamarcomini <annajuliamarcomini23@gmail.com>
2024-04-14 20:15:19 -0300

    feat: discussion component creation

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SelectComponent } from './components/header/select/select.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ContentComponent } from './components/content/content.component';
 import { ResumeComponent } from './components/resume/resume.component';
+import { DiscussionComponent } from './components/discussion/discussion.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { ResumeComponent } from './components/resume/resume.component';
     SidebarComponent,
     ContentComponent,
     ResumeComponent,
+    DiscussionComponent,
   ],
   imports: [BrowserModule],
   providers: [],
