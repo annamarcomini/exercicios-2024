f23488b02cbf32ac1a79448620d9dca8ba03a5d8
annamarcomini <annajuliamarcomini23@gmail.com>
2024-04-14 18:32:50 -0300

    feat: function that changes button color

--- ts/src/app/components/sidebar/sidebar.component.ts
+++ ts/src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+interface Button {
+  label: string;
+  color: string;
+  active: boolean;
+}
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  buttons = [
+    { label: 'Apresentação', color: '#ffff', active: false },
+    { label: 'Comitês', color: '#ffff', active: false },
+    { label: 'Autores', color: '#ffff', active: false },
+    { label: 'Eixos temáticos', color: '#ffff', active: false },
+    { label: 'Trabalhos', color: '#FDF1EB', active: true },
+    { label: 'Contato', color: '#ffff', active: false },
+  ];
 
-  ngOnInit(): void {
+  changeButtonColor(button: Button): void {
+    this.buttons.forEach((b) => {
+      if (b.label === button.label) {
+        b.color = b.active ? '#ffff' : '#FDF1EB'; // Alterna a color do botão clicado
+        b.active = !b.active; // Inverte o estado.active do botão clicado
+      } else {
+        b.color = '#ffff'; // Volta a cor original para os outros botões
+        b.active = false; // Define o estado ativo como falso para os outros botões
+      }
+    });
   }
-
 }
