e7dbd4bd2b5f8cdcb661b01c6a1dba891cc60fda
annamarcomini <annajuliamarcomini23@gmail.com>
2024-04-14 15:29:46 -0300

    feat: comp. to cardi-login works

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CardiLoginComponent } from './components/header/cardi-login/cardi-login.component';
+import { AvatarComponent } from './components/header/avatar/avatar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CardiLoginComponent,
+    AvatarComponent
   ],
   imports: [
     BrowserModule
---
+++ ts/src/app/components/header/cardi-login/cardi-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardi-login',
+  templateUrl: './cardi-login.component.html',
+  styleUrls: ['./cardi-login.component.scss']
+})
+export class CardiLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
