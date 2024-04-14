b0be76af76de3e8db24ef2e2e4e1af5825411edd
annamarcomini <annajuliamarcomini23@gmail.com>
2024-04-14 15:54:17 -0300

    feat: card login props sharing

--- ts/src/app/components/header/avatar/avatar.component.ts
+++ ts/src/app/components/header/avatar/avatar.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-avatar',
   templateUrl: './avatar.component.html',
-  styleUrls: ['./avatar.component.scss']
+  styleUrls: ['./avatar.component.scss'],
 })
 export class AvatarComponent implements OnInit {
+  @Input() notifications!: { user: string; message: string }[];
+  @Input() imageURL!: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- ts/src/app/components/header/card-login/card-login.component.ts
+++ ts/src/app/components/header/card-login/card-login.component.ts
@@ -1,17 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  selector: 'app-card-login',
+  templateUrl: './card-login.component.html',
+  styleUrls: ['./card-login.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class CardLoginComponent implements OnInit {
   @Input() userData!: {
     name: string;
     imageURL: string;
     email: string;
     notifications: { user: string; message: string }[];
   };
+
   constructor() {}
 
   ngOnInit(): void {}
