ea400cf80bba714bd8fcd2e5652c9fc322d99558
annamarcomini <annajuliamarcomini23@gmail.com>
2024-04-14 15:32:32 -0300

    feat: cardi-login mock data

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'DevChuva';
+
+  userData = {
+    imageURL: '/assets/images/png/avatar2.png',
+    name: 'Maria Joaquina',
+    email: 'alguem12@galoascience.com',
+    notifications: [
+      {
+        user: 'Cirilo',
+        message: 'Olá Maria Joaquina',
+      },
+      {
+        user: 'Cirilo',
+        message: 'Passei para ver se esta tudo bem com você?',
+      },
+    ],
+  };
 }
