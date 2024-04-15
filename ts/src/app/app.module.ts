1078d86f49be2ec69eff2fdb41ca96cb0ffde14f
annamarcomini <annajuliamarcomini23@gmail.com>
2024-04-14 23:19:31 -0300

    feat: footer component creation

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DiscussionComponent } from './components/discussion/discussion.componen
 import { AnsweredTopicComponent } from './components/discussion/answered-topic/answered-topic.component';
 import { TopicComponent } from './components/discussion/topic/topic.component';
 import { CommentsComponent } from './components/discussion/comments/comments.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { CommentsComponent } from './components/discussion/comments/comments.com
     AnsweredTopicComponent,
     TopicComponent,
     CommentsComponent,
+    FooterComponent,
   ],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule,],
   providers: [],
