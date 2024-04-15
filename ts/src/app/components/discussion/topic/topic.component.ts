14d19f590d54a2cc6f74d3f30c03f5397fbe3537
annamarcomini <annajuliamarcomini23@gmail.com>
2024-04-14 21:11:44 -0300

    feat: validations and showing success msg function

--- ts/src/app/components/discussion/topic/topic.component.ts
+++ ts/src/app/components/discussion/topic/topic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-topic',
@@ -12,7 +13,35 @@ export class TopicComponent implements OnInit {
     this.showFormTopic = true;
   }
 
+  showSuccessMessage = false;
+
+  handleNewSuccessMsg(): void {
+    this.showSuccessMessage = true;
+  }
+
+  questionForm!: FormGroup;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.questionForm = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      text: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get title() {
+    return this.questionForm.get('title')!;
+  }
+
+  get text() {
+    return this.questionForm.get('text')!;
+  }
+  submit() {
+    if (this.questionForm.invalid) {
+      return;
+    }
+
+    this.handleNewSuccessMsg();
+  }
 }
